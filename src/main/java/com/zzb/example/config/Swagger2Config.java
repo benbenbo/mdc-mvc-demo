10e87516e62a89b15cfc306559a7ba66a9188a55
zhengbingyuan <zhengbingyuan@leyopharm.com>
2022-01-03 18:22:06 +0800

    feat(swagger组件): swagger2换用旧版本

--- src/main/java/com/zzb/example/config/Swagger2Config.java
+++ src/main/java/com/zzb/example/config/Swagger2Config.java
@@ -1,6 +1,15 @@
 package com.zzb.example.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.configuration.SwaggerCommonConfiguration;
+
+import java.util.ArrayList;
 
 /**
  * @author XBird
@@ -8,4 +17,17 @@ import org.springframework.context.annotation.Configuration;
  **/
 @Configuration
 public class Swagger2Config {
+
+    @Bean
+    public Docket docket(){
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo());
+    }
+
+
+    public ApiInfo apiInfo(){
+        //作者信息
+        Contact contact = new Contact("郑炳元", "https://juejin.cn/", "1551907870@qq.com");
+        return new ApiInfo("郑炳元的Swagger文档", "mdc实战使用项目", "v1.0", "https://juejin.cn/",
+                contact, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0", new ArrayList<VendorExtension>());
+    }
 }
--- src/main/java/com/zzb/example/controller/TestController.java
+++ src/main/java/com/zzb/example/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.zzb.example.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,15 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
  * @author XBird
  * @date 2021/12/28
  **/
+@Slf4j
 @RestController
 public class TestController {
     @PostMapping(value="/test")
     public String test(){
+        log.info("访问test接口");
         return "test";
     }
 
     @GetMapping(value="/hello")
     public String hello(){
+        log.info("访问hello接口");
         return "hello";
     }
 }
