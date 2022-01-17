aa0ab32d870b0f33a9752045f6c5214442f1f86b
zhengbingyuan <zhengbingyuan@leyopharm.com>
2022-01-17 10:27:46 +0800

    feat(统计耗时拦截器): 优化代码

--- src/main/java/com/zzb/example/interceptor/TraceInterceptor.java
+++ src/main/java/com/zzb/example/interceptor/TraceInterceptor.java
@@ -1,8 +1,5 @@
 package com.zzb.example.interceptor;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,26 +10,20 @@ import javax.servlet.http.HttpServletResponse;
  * @author zhengbingyuan
  * @date 2022/1/10
  */
-public class TimeTotalInterceptor implements HandlerInterceptor {
-    private static final Logger log = LoggerFactory.getLogger(TimeTotalInterceptor.class);
+public class TraceInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        MDC.put("startTime",String.valueOf(System.currentTimeMillis()));
+        //如果有上层调用就用上层的ID
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        String startTimeStr = MDC.get("startTime");
-        long startTime = Long.parseLong(startTimeStr);
-        long endTime = System.currentTimeMillis();
-        long timePeriod = endTime - startTime;
-        log.info("此次请求耗时：{} 毫秒",timePeriod);
 
-        MDC.remove("startTime");
     }
 }
